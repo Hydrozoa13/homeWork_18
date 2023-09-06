7ba96cd1179252df8fbccae4ac17e2a008495951
Zhenya Loyko <e.lojko@inbox.ru>
2023-09-06 21:43:27 +0300

    Project created

---
+++ homeWork_18/homeWork_18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork_18
+//
+//  Created by Евгений Лойко on 6.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
